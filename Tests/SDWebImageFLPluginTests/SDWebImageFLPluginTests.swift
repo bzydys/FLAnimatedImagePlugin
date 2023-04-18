5f9948a28ba078e1f0f33ba953105a4d640b71dd
mi_李强(Lee.L) <mi_qiang.li@mihoyo.com>
2023-04-18 14:01:58 +0800

    feat: 提交代码

---
+++ Tests/SDWebImageFLPluginTests/SDWebImageFLPluginTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SDWebImageFLPlugin
+
+final class SDWebImageFLPluginTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SDWebImageFLPlugin().text, "Hello, World!")
+    }
+}
